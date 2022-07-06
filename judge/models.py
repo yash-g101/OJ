da0ae56dd8f148ca33b56a819148af2ee2358120
Yash <yash.ghodekar101@gmail.com>
2022-07-06 22:47:04 +0530

    Created views for problem_list, Testcases, submissions and leaderboard

--- judge/models.py
+++ judge/models.py
@@ -4,15 +4,21 @@ from django.db import models
 class Problem(models.Model):
     problem_title = models.CharField(max_length=25)
     problem_statement = models.TextField()
+
+    def __str__(self):
+        return self.problem_title
     
 class Testcases(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
     test_input = models.TextField()
     test_output = models.TextField()
 
+    def __str__(self):
+        return self.test_input, self.test_output
+
 VERDICT_CHOICES = (
     ("0", "Unattempted"),
-    ("1", "Correct Answer"),
+    ("1", "Accepted"),
     ("2", "WA"), 
     ("3", "TLE"),
     ("4", "CTE"),
@@ -29,5 +35,8 @@ LANGUAGE_CHOICES = (
 class Submissions(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
     language = models.CharField(max_length=20, choices=LANGUAGE_CHOICES, default="C++")
-    submission = models.TextField()
+    submitted_code = models.TextField()
     verdict = models.CharField(max_length=40, choices=VERDICT_CHOICES)
+
+    def __str__(self):
+        return self.verdict
--- judge/views.py
+++ judge/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Problem, Submissions, Testcases
+import os, filecmp
 
 def index(request):
-    return HttpResponse("Hello !!")
+    problem_list = Problem.objects.all()
+    return render(request, "judge/index.html", {'problem_list': problem_list})
+
+def tests(request, problem_id):
+    problem = get_object_or_404(Problem, pk=problem_id)
+    return render(request, 'judge/testcases.html', {'problem': problem})
+
+def answers(request, problem_id):
+    problem = get_object_or_404(Problem, pk=problem_id)
+    return render(request, 'judge/submissions.html', {'problem': problem})
+
+def submit(request, problem_id):
+    problem = get_object_or_404(Problem, pk=problem_id)
+    f = request.FILES('sol')
+    with open('/Users/Yash/Desktop/Output/sol.cpp', 'wb+') as dest:
+        for chunk in f.chunks():
+            dest.write(chunk)
+    os.system('g++ /Users/Yash/Desktop/folder/sol.cpp')
+    os.system('./a.out < /Users/Yash/Desktop/folder/inp.txt > /Users/Yash/Desktop/folder/out.txt')
+
+    out1 = '/Users/Yash/Desktop/folder/out.txt'
+    out2 = 'Users/Yash/Desktop/folder/actual_out.txt'
+    if(filecmp.cmp(out1, out2, shallow=False)):
+        verdict = "Accepted"
+    else:
+        verdict = "WA"
+    
+    submission = Submissions()
+    submission.problem = Problem.objects.get(pk=problem_id)
+    submission.verdict = verdict
+    submission.submitted_code = '/Users/Yash/Desktop/folder/sol.cpp'
+    submission.language = "C++"
+    submission.save()
+
+    return HttpResponseRedirect(reverse('judge:leaderboard'))
+
+def leaderboard(request):
+    submissions = Submissions.objects.all()
+    return render(request, 'judge/leaderboard.html', {'submissions': submissions})
